94d127d984d103a33c6c201fdedb74519e178051
Aman Ayoub <aman.ayoub16@gmail.com>
2024-04-10 14:52:02 +0430

    how big is the room?

--- exercises/easy_1/room_area.js
+++ exercises/easy_1/room_area.js
@@ -1,4 +1,5 @@
 const readline = require("readline-sync");
+const SQ_METER_TO_SQFEETS = 10.7639;
 
 console.log("Enter the length of the room in meters:");
 let lengthInMeter = Number(readline.prompt());
@@ -7,7 +8,7 @@ console.log("Enter the width of the room in meters:");
 let widthInMeter = Number(readline.prompt());
 
 let roomInMeters = lengthInMeter * widthInMeter;
-let roomInFeets = roomInMeters * 10.7639;
+let roomInFeets = roomInMeters * SQ_METER_TO_SQFEETS;
 
 console.log(`The area of the room is ${roomInMeters.toFixed(2)} square meters (${roomInFeets.toFixed(2)} square feet).`);
 
