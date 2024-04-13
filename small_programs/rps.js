a398d736da85a8f43c1b403327346f9c712dd0ce
Aman Ayoub <aman.ayoub16@gmail.com>
2024-04-13 06:54:19 +0430

    walk-through: rock paper scissors

--- small_programs/rps.js
+++ small_programs/rps.js
@@ -5,15 +5,51 @@ function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-prompt(`Choose one of: ${VALID_CHOICES.join(', ')}`);
-let choice = readline.prompt();
-
-while (!VALID_CHOICES.includes(choice)) {
-  prompt("That's not a valid choice");
-  choice = readline.prompt();
+function displayWinner(choice, computerChoice) {
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+      (choice === 'paper' && computerChoice === 'rock') ||
+      (choice === 'scissors' && computerChoice === 'paper')) {
+    prompt('You won!');
+  } else if ((choice === 'scissors' && computerChoice === 'rock') ||
+            (choice === 'rock' && computerChoice === 'paper') ||
+            (choice === 'paper' && computerChoice === 'scissors')) {
+    prompt('Computer won!');
+  } else {
+    prompt("It's a tie!");
+  }
 }
 
-let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-let computerChoice = VALID_CHOICES[randomIndex];
+let keepGoing = true;
+while (keepGoing) {
+  // Get the user choice:
+  prompt(`Choose one of: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.prompt();
 
-prompt(`You chose ${choice}. Computer chose ${computerChoice}.`);
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.prompt();
+  }
+
+  // Get the computer choice:
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+  // Display who the winner is:
+  displayWinner(choice, computerChoice);
+
+  prompt("Would you like to play again? (y/n)");
+  let answer = readline.prompt().toLowerCase();
+  prompt(answer);
+
+  while (answer[0] !== 'y' && answer[0] !== 'n') {
+    prompt("Please choose 'y' or 'n'");
+    answer = readline.prompt();
+  }
+
+  // Terminate the loop:
+  if (answer[0] === 'n') {
+    keepGoing = false;
+  }
+}
