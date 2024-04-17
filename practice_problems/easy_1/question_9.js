1a630a81308865c7db00bf36c2281f793d8b0cad
Aman Ayoub <aman.ayoub16@gmail.com>
2024-04-17 10:39:28 +0430

    adding 2 items to an array at once

---
+++ practice_problems/easy_1/question_9.js
@@ -0,0 +1,10 @@
+// Question: 
+// How can we add multiple items to our array? ('Dino' and 'Hoppy')
+let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+
+// Solution:
+// We can do so by adding more arguments to the push() method:
+flintstones.push("Dino", "Hoppy");
+
+console.log(flintstones);
+// Logs: ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles", "Dino", "Hoppy"]
