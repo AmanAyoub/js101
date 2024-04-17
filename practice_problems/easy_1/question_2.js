d18ac698a5f3dfecb91472805a68ddb1c4a360a3
Aman Ayoub <aman.ayoub16@gmail.com>
2024-04-17 10:32:18 +0430

    string contains exclamation

--- practice_problems/easy_1/question_2.js
+++ practice_problems/easy_1/question_2.js
@@ -1,4 +1,4 @@
-// Question:
+// Question: 
 // How can you determine whether a given string ends with an exclamation mark (!)?
 let str1 = "Come over here!"; // true
 let str2 = "What's up, Doc?"; // false
