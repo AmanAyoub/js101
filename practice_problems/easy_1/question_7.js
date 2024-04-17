4316c159d0aeca77af8dc9792b1707b8e324ef52
Aman Ayoub <aman.ayoub16@gmail.com>
2024-04-17 10:38:04 +0430

    include() method

--- practice_problems/easy_1/question_7.js
+++ practice_problems/easy_1/question_7.js
@@ -1,3 +1,4 @@
+// Question:
 // Determine whether the name Dino appears in the strings below -- check each string separately:
 let str1 = "Few things in life are as important as house training your pet dinosaur.";
 let str2 = "Fred and Wilma have a pet dinosaur named Dino.";
