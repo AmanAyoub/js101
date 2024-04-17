2568eefd52c6c84f873bf99107f299b5c96c6207
Aman Ayoub <aman.ayoub16@gmail.com>
2024-04-17 10:34:04 +0430

    fixing string capitalizition

--- practice_problems/easy_1/question_4.js
+++ practice_problems/easy_1/question_4.js
@@ -1,4 +1,4 @@
-// Question:
+// Question: 
 // Using the following string, create a new string that contains all lowercase
 // letters except for the first character, which should be capitalized.
 let munstersDescription = "the Munsters are CREEPY and Spooky.";
