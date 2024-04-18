cef09dcbc3a611d22d257f0c08085d31eefaff39
Aman Ayoub <aman.ayoub16@gmail.com>
2024-04-18 10:25:42 +0430

    splice() method

---
+++ practice_problems/easy_2/question_5.js
@@ -0,0 +1,10 @@
+// Question:
+// Given an array of numbers [1, 2, 3, 4, 5], mutate the array by
+// removing the number at index 2, so that the array becomes [1, 2, 4, 5].
+
+// Solution:
+let numbers = [1, 2, 3, 4, 5];
+numbers.splice(2, 1);
+
+console.log(numbers);
+// Logs: [1, 2, 4, 5]
