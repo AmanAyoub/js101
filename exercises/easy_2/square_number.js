bbcff53aeff5d84be449f745e805412bff981042
Aman Ayoub <aman.ayoub16@gmail.com>
2024-05-10 09:37:17 +0430

    squaring an argument

---
+++ exercises/easy_2/square_number.js
@@ -0,0 +1,9 @@
+// write a function that computes the square of its argument.
+
+function square(number) {
+  return number * number;
+}
+
+console.log(square(5) === 25); // logs true
+console.log(square(-8) === 64); // logs true
+
